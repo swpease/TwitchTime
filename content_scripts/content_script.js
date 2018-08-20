9b58bc4636dde6566634bb401320850a642768b1
swpease <swpease@gmail.com>
2018-08-20 14:40:19 -0700

    [content script] main() skeleton
    Have the logical ordering of the method. Need to define the methods it will call.

--- content_scripts/content_script.js
+++ content_scripts/content_script.js
@@ -1,3 +1,5 @@
+var CURRENT_CHANNEL = "";
+
 /**
  * Gets the Twitch channel name if available. Raises an Error if name not found.
  * @return {string} The channel name.
@@ -19,5 +21,24 @@ function getChannelName() {
 
   } else {
     throw new Error("Channel name not found.");
+  }
+}
+
+/**
+ * If it can find a channel name, it either:
+ *  - calls a function to update the time spent watching
+ *  - calls a function to set stuff up for a newly navigated-to channel
+ * Otherwise, it resets the `current channel`.
+ */
+function main() {
+  try {
+    const channelName = getChannelName();
+    if (channelName === CURRENT_CHANNEL) {
+      return
+    } else {
+      return
+    }
+  } catch (e) {
+    CURRENT_CHANNEL = "";
   }
 }
