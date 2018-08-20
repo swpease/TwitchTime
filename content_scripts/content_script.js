42484adb5deb1e9faebb6e8fae6e78ce6352d819
swpease <swpease@gmail.com>
2018-08-20 14:12:30 -0700

    [content script] getChannelName return null -> throw Error
    I decided to change from returning null to raising an error, since it seems a bit more logically easier, at least in this case, I'll just use a try catch block instead of an if return is not null.

--- content_scripts/content_script.js
+++ content_scripts/content_script.js
@@ -1,7 +1,6 @@
 /**
- * Gets the Twitch channel name if available. Works if you are able to watch
- * a video from the current url.
- * @return {(string|null)} The channel name or null.
+ * Gets the Twitch channel name if available. Raises an Error if name not found.
+ * @return {string} The channel name.
  */
 function getChannelName() {
   const pathname = window.location.pathname;
@@ -15,8 +14,10 @@ function getChannelName() {
   if (splitPath[1] === "videos" || splitPath.length === 2) {
     const channelNameElement = document.querySelector("main h5");  // Brittle.
     const channelName = channelNameElement.textContent.toLowerCase();  // Protect from display name changes.
+
     return channelName;
+
   } else {
-    return null;
+    throw new Error("Channel name not found.");
   }
 }
