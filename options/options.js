2a1501a02215f1588ad9e938ff57f6f7bcca032f
swpease <swpease@gmail.com>
2019-07-29 18:52:20 -0700

    [options] settings page implemented
    I don't think there's anything else to do on this end. I just kinda copied the default example from the Mozilla docs.

---
+++ options/options.js
@@ -0,0 +1,26 @@
+/**
+ * Saves the preferred format for what is displayed on the Twitch webpage.
+ */
+function saveDisplayFormat(e) {
+  browser.storage.sync.set({
+    displayFormat: document.querySelector("#tt-display").value
+  });
+
+  e.preventDefault();
+}
+
+/**
+ * Sets the <select> to the current saved format.
+ */
+function restoreDisplayFormat() {
+  let gettingFormat = browser.storage.sync.get({ displayFormat: "watched-dh" });
+
+  gettingFormat.then((res) => {
+    document.querySelector("#tt-display").value = res.displayFormat;
+  }, (error) => {
+    console.log(`Error: ${error}`);
+  });
+}
+
+document.addEventListener('DOMContentLoaded', restoreDisplayFormat);
+document.querySelector("form").addEventListener("submit", saveDisplayFormat);
