af2ef80d1eff61809681404a996dd8f006304d9d
swpease <swpease@gmail.com>
2019-07-31 11:45:57 -0700

    [popup] basic display
    Shows the watched channels in a table in the popup. Currently, I cutoff the channels to show at 30 minutes. I might want to let it be customizable later, but it seemed like a decent default.
    
    I think I want to redo the formatting on the injected div for the time display on the Twitch page. Maybe just dd:hh:mm

---
+++ popup/popup.js
@@ -0,0 +1,54 @@
+function onError(error) {
+    console.log(error);
+}
+
+/**
+ * Converts time in seconds to the following form:
+ * [d*]d:hh:mm
+ * @param {number} time - Time, in seconds.
+ * @return {string} A formatted time string.
+ */
+function formatTime(time) {
+  let formattedTime = "";
+
+  const timeInHours = Math.floor(time / 3600);
+  const days = Math.floor(timeInHours / 24);
+  const hours = timeInHours % 24;
+  const timeInMinutes = Math.floor(time / 60);
+  const minutes = timeInMinutes % 60;
+
+  const formattedDays = days === 0 ? "" : `${days}:`;
+  const formattedHours = hours < 10 ? `0${hours}:` : `${hours}:`;
+  const formattedMinutes = minutes < 10 ? `0${minutes}` : `${minutes}`;
+  formattedTime += formattedDays + formattedHours + formattedMinutes;
+
+  return formattedTime;
+}
+
+function populateTable(data) {
+  let dataList = [];
+  for (let [k, v] of Object.entries(data)) {
+    dataList.push({ name: k, time: v });
+  }
+
+  const reservedWords = ["displayFormat"];
+  let channels = dataList.filter(datum => !reservedWords.includes(datum.name));
+  let topChannels = channels.filter(datum => datum.time > 1800);  // TODO: options besides 30 mins?
+  topChannels.sort((a, b) => b.time - a.time);
+
+  let timeTable = document.getElementById("time-table");
+  topChannels.forEach(function(channel) {
+      let row = timeTable.insertRow(-1);
+      let channelCell = row.insertCell(0);
+      let timeCell = row.insertCell(1);
+
+      let link = document.createElement("a");
+      link.href = "https://twitch.tv/" + channel.name;
+      link.innerHTML = channel.name;
+
+      channelCell.appendChild(link);
+      timeCell.innerHTML = formatTime(channel.time);
+  });
+}
+
+browser.storage.sync.get().then(populateTable, onError);
