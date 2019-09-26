34117318d12ddab3bc0cced8bb27bb586e5ebf63
swpease <swpease@gmail.com>
2019-09-26 10:39:49 -0700

    [content_script] channel name in URL
    Changed getChannelName() to reflect the Twitch update. The URLs are now more logically organized, where things under a channel are like: `twitch.tv/chocotaco/clips` instead of whatever randomness they had before.
    
    I added in a list of reserved words for settings, etc. A quick look indicated that these are not any streamers, and I didn't want to pollute the history with false data.

--- content_scripts/content_script.js
+++ content_scripts/content_script.js
@@ -153,18 +153,11 @@ function addWatchTime() {
 function getChannelName() {
   const pathname = window.location.pathname;
   const splitPath = pathname.split("/");
-  /*
-   * To my knowledge, the only paths you can start to watch videos from are:
-   *   1. twitch.tv/[channel]
-   *   2. twitch.tv/videos/*
-   * As such, these are the only paths I'll try to find the channel name from.
-   */
-  if (splitPath[1] === "videos" || splitPath.length === 2) {
-    const channelNameElement = document.querySelector("main h5");  // Brittle.
-    const channelName = channelNameElement.textContent.toLowerCase();  // *display* names have capitals, not account names.
+  const reservedWords = ["friends", "subscriptions", "inventory",
+                         "payments", "settings"]
 
-    return channelName;
-
+  if (splitPath.length >= 2 && !reservedWords.includes(splitPath[1])) {
+    return splitPath[1];
   } else {
     throw new Error("Channel name not found.");
   }
